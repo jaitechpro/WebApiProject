78d43498ffdd9d18708cb3b4240627abceaa78bc
Jairam <Jairam@DESKTOP-KJOMPRU>
2022-01-31 17:25:16 +0530

    Add project files.

---
+++ WebApiProject/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApiProject.Models;
+
+namespace WebApiProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext _context;
+
+        public StudentController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Register(Student a)
+        {
+            _context.Students.Add(a);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpPost("Update")]
+        public void Update(Student b)
+        {
+            _context.Students.Update(b);
+            _context.SaveChanges();
+        }
+        [HttpDelete("Delete")]
+        public async Task<ActionResult<bool>> Delete(Student b)
+        {
+            _context.Students.Remove(b);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpGet("GetData")]
+        public async Task<List<Student>> GetData()
+        {
+            var data = await _context.Students.ToListAsync();
+            return data;
+
+        }
+
+    }
+}
---
+++ WebApiProject/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiProject.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public int Password { get; set; }
+
+    }
+}
